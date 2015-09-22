0a88d6d2ffdf47acfc81fd4c09145a5d5b992398
Ian Storz <i@ianstorz.com>
2015-09-21 22:26:23 -0700

    Second test passing

--- bob.exs
+++ bob.exs
@@ -1,6 +1,7 @@
 defmodule Teenager do
   def hey(input) do
     cond do
+      Regex.match?(~r/^(?:[^A-Za-z]*[A-Z][^\s,]*)*[^A-Za-z]*$/, input) -> "Whoa, chill out!"
       true -> "Whatever."
     end
   end
