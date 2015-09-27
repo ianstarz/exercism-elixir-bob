a459474a49d3a41853afd6eca659ddd965e0a676
Ian Storz <i@ianstorz.com>
2015-09-27 12:13:57 -0700

    Second submission

--- bob.exs
+++ bob.exs
@@ -1,8 +1,8 @@
 defmodule Teenager do
   def hey(input) do
     cond do
-      !List.last(String.split(input)) -> "Fine. Be that way!"
-      Regex.match?(~r/.*\?$/, input) -> "Sure."
+      String.ends_with?(input, "?") -> "Sure."
+      String.strip(input) == "" -> "Fine. Be that way!"
       String.upcase(input) == input && Regex.match?(~r/[\p{Lu}]/, input) -> "Whoa, chill out!"
       true -> "Whatever."
     end
